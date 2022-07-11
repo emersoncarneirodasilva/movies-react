52eeea0b69124e72ca82f2df00aee978f6294445
Emerson Carneiro da Silva <94311606+emersoncarneirodasilva@users.noreply.github.com>
2022-07-11 09:50:54 -0300

    Update Search.jsx

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -31,6 +31,7 @@ const Search = () => {
         Resultados para: <span className="query-text">{query}</span>
       </h2>
       <div className="movies-container">
+        {movies.length === 0 && <p>Carregando...</p>}
         {movies.length > 0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
     </div>
